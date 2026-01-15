88e6a6bd58c025ae7ff1746bedfad560a08a5736
beran4-2 <beran2@spsejecna.cz>
2026-01-15 23:15:45 +0100

    oprava

--- src/Inventar/Inventar.java
+++ src/Inventar/Inventar.java
@@ -1,6 +1,12 @@
 package Inventar;
 
+import Predmety.Predmet;
+
+import java.util.ArrayList;
+
 public class Inventar {
+
+    ArrayList<Predmet> inventar;
 
     public void pridatPredmet() {
     }
