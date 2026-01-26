dac406604ba3443e37f7dc6fade5e90a05c0848e
beran4-2 <beran2@spsejecna.cz>
2026-01-26 16:08:24 +0100

    přidání konzole a komandu pomoc

--- src/Commands/Pomoc.java
+++ src/Commands/Pomoc.java
@@ -4,8 +4,7 @@ import Konzole.Konzole;
 
 public class Pomoc implements Command {
     public String vykonat(Konzole konzole, String string) {
-
-        return "";
+        return "pomoc, konec, mluvit, jdi dal/zpatky, daydream souboj/ukryt, průzkum, úkoly";
     }
     public boolean odejit() {
         return false;
--- src/Konzole/Konzole.java
+++ src/Konzole/Konzole.java
@@ -1,8 +1,106 @@
 package Konzole;
 
+import Commands.Command;
+import Commands.Pomoc;
+import HerniNacitani.HerniNacitani;
+import Commands.Pohyb;
+import Lokace.Lokace;
+
+import java.util.HashMap;
+import java.util.Scanner;
+
+
 public class Konzole {
+    private boolean konecHry;
+    private Scanner scanner = new Scanner(System.in);
+    private HashMap<String, Command> mapaPrikazu = new HashMap<>();
+    private HerniNacitani data;
+    private Lokace aktualniLokace;
 
-    public void Hra(){
+    public void hra(){
+        konecHry = false;
+        data = HerniNacitani.nactiDataZeSlozky("/herniSvet.json");
+        aktualniLokace = data.getLokace().get(0);
+        inicializace();
+        System.out.println("Prave jsi v lokaci " + aktualniLokace);
+        //data.vypisLokaci();
 
+        do{
+            provedPrikaz();
+        }while(!konecHry);
+
+
+    }
+
+
+
+    private void inicializace() {
+        mapaPrikazu.put("jdi", new Pohyb());
+        mapaPrikazu.put("pomoc", new Pomoc());
+    }
+
+    private void provedPrikaz() {
+        System.out.print(">>");
+        String prikaz = scanner.nextLine();
+        prikaz = prikaz.trim();
+        String[] rozdeleni = prikaz.split(" ");
+        String prikaz1 = rozdeleni[0].toLowerCase();
+        String prikaz2 = "";
+        if (rozdeleni.length  == 2) {
+            prikaz2 = rozdeleni[1].toLowerCase();
+        }
+        if (mapaPrikazu.containsKey(prikaz1)) {
+            mapaPrikazu.get(prikaz1).vykonat(this,prikaz2);
+        } else {
+            System.out.println(">> Nedefinovany prikaz");
+        }
+    }
+
+    public Lokace getAktualniLokace() {
+        return aktualniLokace;
+    }
+
+
+
+
+
+
+
+
+
+    public boolean isKonecHry() {
+        return konecHry;
+    }
+
+    public void setKonecHry(boolean konecHry) {
+        this.konecHry = konecHry;
+    }
+
+    public Scanner getScanner() {
+        return scanner;
+    }
+
+    public void setScanner(Scanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public HashMap<String, Command> getMapaPrikazu() {
+        return mapaPrikazu;
+    }
+
+    public void setMapaPrikazu(HashMap<String, Command> mapaPrikazu) {
+        this.mapaPrikazu = mapaPrikazu;
+    }
+
+    public HerniNacitani getData() {
+        return data;
+    }
+
+    public void setData(HerniNacitani data) {
+        this.data = data;
+    }
+
+    public void setAktualniLokace(Lokace aktualniLokace) {
+        this.aktualniLokace = aktualniLokace;
     }
 }
