a89b622766c9a0403691263b0709ae5ef16670d6
beran4-2 <beran2@spsejecna.cz>
2026-01-26 13:36:17 +0100

    V tride lokace je novy ArrayList

--- src/Lokace/Lokace.java
+++ src/Lokace/Lokace.java
@@ -1,21 +1,27 @@
 package Lokace;
 
+import Postavy.Postavy;
+
+import java.util.ArrayList;
+
 public class Lokace {
     private String nazev;
     private boolean ukolHotovy;
     private String popis;
     private String naslednik;
+    private ArrayList<Postavy> postavyVLokaci;
 
 
     public void dejVMistonosti() {
 
     }
 
-    public Lokace(String nazev, boolean ukolHotovy, String popis, String naslednik) {
+    public Lokace(String nazev, boolean ukolHotovy, String popis, String naslednik, ArrayList<Postavy> postavyVLokaci) {
         this.nazev = nazev;
         this.ukolHotovy = ukolHotovy;
         this.popis = popis;
         this.naslednik = naslednik;
+        this.postavyVLokaci = postavyVLokaci;
     }
 
     @Override
