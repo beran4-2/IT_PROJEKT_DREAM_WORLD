fafeeef5475d3b93dd1a16eef52faf935112dc22
beran4-2 <beran2@spsejecna.cz>
2026-01-26 13:36:33 +0100

    Pridane nove tridy komunikace

---
+++ src/Komunikace/Odpoved.java
@@ -0,0 +1,7 @@
+package Komunikace;
+
+public class Odpoved {
+    private int id;
+    private String odpoved;
+    private int IdOtazky;
+}
---
+++ src/Komunikace/Otazka.java
@@ -0,0 +1,7 @@
+package Komunikace;
+
+public class Otazka {
+    private int id;
+    private String otazka;
+    private int idOdpovedi;
+}
